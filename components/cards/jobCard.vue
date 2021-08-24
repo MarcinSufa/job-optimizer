4be8b968d81e905e6cf85a66e3e59fc196be2ac0
Marcin Sufa <sufa.marcin@gmail.com>
2021-08-24 22:12:09 +0200

    adjust card styles

--- components/cards/jobCard.vue
+++ components/cards/jobCard.vue
@@ -14,129 +14,123 @@
         type="error"
         >{{ jobData.error.message }}</v-alert
       >
-      <div class="job-position-card-inner">
-        <div>
+      <div>
+        <v-fab-transition>
+          <v-btn
+            class="map-center-button"
+            color="primary"
+            absolute
+            depressed
+            elevation="2"
+            fab
+            icon
+            rounded
+            x-small
+            @click="$emit('centerToMarker', jobData.latLng)"
+          >
+            <v-icon>mdi-map-marker-radius</v-icon>
+          </v-btn>
+        </v-fab-transition>
+
+        <v-card-title class="text-subtitle-1 pt-0 pb-2 font-weight-medium pl-0">
           <v-img
+            class="mr-3"
             contain
             max-height="64"
             max-width="64"
             :src="jobData.companyLogo"
           ></v-img>
-        </div>
-        <div class="job-position-card-inner-content">
-          <div>
-            <v-fab-transition>
-              <v-btn
-                class="map-center-button"
-                color="primary"
-                absolute
-                depressed
-                elevation="2"
-                fab
-                icon
-                rounded
-                x-small
-                @click="$emit('centerToMarker', jobData.latLng)"
-              >
-                <v-icon>mdi-map-marker-radius</v-icon>
-              </v-btn>
-            </v-fab-transition>
+          {{ jobData.jobTitle }} - {{ jobData.company }}
+          <div class="job-position-card-salary text-subtitle-2 ml-4">
+            {{ jobData.salary.min | formatShortThousandNumber }} -
+            {{ jobData.salary.max | formatShortThousandNumber }}
+            real salary
+            {{ displayRealSalary | formatShortThousandNumber }}
+          </div>
+        </v-card-title>
 
-            <v-card-title class="text-subtitle-1 pt-2 pb-2 font-weight-medium">
-              {{ jobData.jobTitle }} - {{ jobData.company }}
-            </v-card-title>
-            <div class="job-position-card-salary text-subtitle-2 ml-4">
-              {{ jobData.salary.min | formatShortThousandNumber }} -
-              {{ jobData.salary.max | formatShortThousandNumber }}
-              real salary
-              {{ displayRealSalary | formatShortThousandNumber }}
-            </div>
-            <v-tabs
-              v-model="tab"
-              background-color="transparent"
-              color="basil"
-              grow
-            >
-              <v-tab v-for="item in items" :key="item">
-                {{ item }}
-              </v-tab>
-            </v-tabs>
-            <v-tabs-items v-model="tab">
-              <v-tab-item key="Description">
-                <v-card-text>
+        <v-tabs v-model="tab" background-color="transparent" color="basil" grow>
+          <v-tab v-for="item in items" :key="item">
+            {{ item }}
+          </v-tab>
+        </v-tabs>
+        <v-tabs-items v-model="tab">
+          <v-tab-item key="Description">
+            <v-card-text>
+              <div>
+                {{ jobData.description }}
+              </div>
+              <br />
+              <v-row no-gutters>
+                <v-col>
                   <div>
-                    {{ jobData.description }}
+                    <v-icon small color="primary" class="mr-3">mdi-home</v-icon>
+                    {{ jobData.city }}
                   </div>
                   <br />
-                  <v-row no-gutters>
-                    <v-col>
-                      <div>
-                        <v-icon small color="primary" class="mr-3"
-                          >mdi-home</v-icon
-                        >
-                        {{ jobData.city }}
-                      </div>
-                      <br />
-                      <div>
-                        <v-icon small color="primary" class="mr-3"
-                          >mdi-cash-multiple</v-icon
-                        >
-                        {{ jobData.contract }}
-                      </div>
-                    </v-col>
-                    <v-col>
-                      <v-icon small color="primary" class="mr-3"
-                        >mdi-map-marker</v-icon
-                      >
-                      {{ jobData.address }}
-                    </v-col>
-                  </v-row>
-                </v-card-text>
-              </v-tab-item>
-              <v-tab-item key="Salary Calculations">
-                <v-card-text>
-                  <v-row no-gutters>
-                    <v-col>
-                      <div>
-                        <br />
-                        <p ref="timeCounter" class="timerCounter">
-                          commute time: {{ displayTravelTime }} minutes
-                        </p>
-                        distance {{ displayTravelDistance }} km
-                      </div>
-                      <div v-if="jobData.scoring.enabled">
-                        Real Salary:
-                        {{ displayRealSalary | formatShortThousandNumber }}
-                      </div>
-                    </v-col>
-                    <v-col>
-                      <div>
-                        <br />
-                        <p class="timerCounter">
-                          unpaid holidays cost:
-                          {{ jobData.scoring.salary.holidays }} yearly
-                        </p>
-                      </div>
-                    </v-col>
-                  </v-row>
-                </v-card-text>
-              </v-tab-item>
-              <v-tab-item key="Technology">
-                <v-card-text>
-                  <v-chip
-                    v-for="tech in jobData.tech"
-                    :key="tech"
-                    class="ma-2"
-                    color="primary"
-                    outlined
-                    pill
-                    >{{ tech }}</v-chip
+                  <div>
+                    <v-icon small color="primary" class="mr-3"
+                      >mdi-cash-multiple</v-icon
+                    >
+                    {{ jobData.contract }}
+                  </div>
+                </v-col>
+                <v-col>
+                  <v-icon small color="primary" class="mr-3"
+                    >mdi-map-marker</v-icon
                   >
-                </v-card-text>
-              </v-tab-item>
-            </v-tabs-items>
-          </div>
-        </div>
+                  {{ jobData.address }}
+                </v-col>
+              </v-row>
+            </v-card-text>
+          </v-tab-item>
+          <v-tab-item key="Salary Calculations">
+            <v-card-text>
+              <v-row no-gutters>
+                <v-col>
+                  <div>
+                    <br />
+                    <p ref="timeCounter" class="timerCounter">
+                      commute time: {{ displayTravelTime }} minutes
+                    </p>
+                    distance {{ displayTravelDistance }} km
+                  </div>
+                </v-col>
+                <v-col>
+                  <div>
+                    <br />
+                    <p class="timerCounter">
+                      unpaid holidays cost:
+                      {{
+                        jobData.scoring.salary.holidays
+                          | formatShortThousandNumber
+                      }}
+                      yearly
+                    </p>
+                  </div>
+                  <div v-if="jobData.scoring.enabled">
+                    Real Salary:
+                    {{ displayRealSalary | formatShortThousandNumber }}
+                  </div>
+                </v-col>
+              </v-row>
+            </v-card-text>
+          </v-tab-item>
+          <v-tab-item key="Technology">
+            <v-card-text>
+              <v-chip
+                v-for="tech in jobData.tech"
+                :key="tech"
+                class="ma-2"
+                color="primary"
+                outlined
+                pill
+                >{{ tech }}</v-chip
+              >
+            </v-card-text>
+          </v-tab-item>
+        </v-tabs-items>
+
         <v-card-actions class="text-right job-position-card-action-buttons">
           <v-spacer />
           <v-btn
@@ -239,11 +233,6 @@ export default {
   margin-bottom: 1rem;
   &-salary {
     color: #41b883;
-  }
-  &-inner {
-    display: flex;
-    &-content {
-    }
   }
   &-active {
     border: 1px deeppink solid;
